174aba4351ab05afa1cd675631f631ab1440421d
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-08-22 17:16:15 +0300

    Added diagrams for usecases

--- internal/errors/device.go
+++
@@ -1,7 +0,0 @@
-package errors
-
-import "errors"
-
-var (
-	ErrDeviceNotFound = errors.New("device not found")
-)
--- internal/errors/user.go
+++
@@ -1,9 +0,0 @@
-package errors
-
-import "errors"
-
-var (
-	ErrUserAlreadyExists = errors.New("user already exists")
-	ErrUserNotFound      = errors.New("user not found")
-	ErrInvalidPassword   = errors.New("invalid password")
-)
